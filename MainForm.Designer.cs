1d34422c67e30b838567961e26042902689845e2
User <kracnai.ximia@gmail.com>
2025-02-13 13:41:33 +0500

    Первое окно работает как надо, но на форме игрового поля нет и на форме паузы тоже нет кнопок

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,27 +1,105 @@
 ﻿namespace Saper.Forms
 {
     partial class MainForm
-{
-    private System.ComponentModel.IContainer components = null;
-
-    protected override void Dispose(bool disposing)
     {
-        if (disposing && (components != null))
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnEasy;
+        private System.Windows.Forms.Button btnMedium;
+        private System.Windows.Forms.Button btnHard;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnRules;
+        private System.Windows.Forms.Button btnLeaderboard;
+
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
-    }
 
-    private void InitializeComponent()
-    {
-        this.SuspendLayout();
-        // 
-        // MainForm
-        // 
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Name = "MainForm";
-        this.ResumeLayout(false);
+        private void InitializeComponent()
+        {
+            this.btnEasy = new System.Windows.Forms.Button();
+            this.btnMedium = new System.Windows.Forms.Button();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRules = new System.Windows.Forms.Button();
+            this.btnLeaderboard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEasy
+            // 
+            this.btnEasy.Location = new System.Drawing.Point(100, 50);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(100, 30);
+            this.btnEasy.TabIndex = 0;
+            this.btnEasy.Text = "Легкий уровень";
+            this.btnEasy.UseVisualStyleBackColor = true;
+            this.btnEasy.Click += new System.EventHandler(this.btnEasy_Click);
+            // 
+            // btnMedium
+            // 
+            this.btnMedium.Location = new System.Drawing.Point(100, 100);
+            this.btnMedium.Name = "btnMedium";
+            this.btnMedium.Size = new System.Drawing.Size(100, 30);
+            this.btnMedium.TabIndex = 1;
+            this.btnMedium.Text = "Средний уровень";
+            this.btnMedium.UseVisualStyleBackColor = true;
+            this.btnMedium.Click += new System.EventHandler(this.btnMedium_Click);
+            // 
+            // btnHard
+            // 
+            this.btnHard.Location = new System.Drawing.Point(100, 150);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(100, 30);
+            this.btnHard.TabIndex = 2;
+            this.btnHard.Text = "Сложный уровень";
+            this.btnHard.UseVisualStyleBackColor = true;
+            this.btnHard.Click += new System.EventHandler(this.btnHard_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(100, 200);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 30);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnRules
+            // 
+            this.btnRules.Location = new System.Drawing.Point(100, 250);
+            this.btnRules.Name = "btnRules";
+            this.btnRules.Size = new System.Drawing.Size(100, 30);
+            this.btnRules.TabIndex = 4;
+            this.btnRules.Text = "Правила";
+            this.btnRules.UseVisualStyleBackColor = true;
+            this.btnRules.Click += new System.EventHandler(this.btnRules_Click);
+            // 
+            // btnLeaderboard
+            // 
+            this.btnLeaderboard.Location = new System.Drawing.Point(100, 300);
+            this.btnLeaderboard.Name = "btnLeaderboard";
+            this.btnLeaderboard.Size = new System.Drawing.Size(100, 30);
+            this.btnLeaderboard.TabIndex = 5;
+            this.btnLeaderboard.Text = "Таблица лидеров";
+            this.btnLeaderboard.UseVisualStyleBackColor = true;
+            this.btnLeaderboard.Click += new System.EventHandler(this.btnLeaderboard_Click);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(300, 400);
+            this.Controls.Add(this.btnEasy);
+            this.Controls.Add(this.btnMedium);
+            this.Controls.Add(this.btnHard);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRules);
+            this.Controls.Add(this.btnLeaderboard);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+        }
     }
-}
 }
--- MainForm.cs
+++ MainForm.cs
@@ -10,10 +10,41 @@ namespace Saper.Forms
             InitializeComponent();
         }
 
+        private void btnEasy_Click(object sender, EventArgs e)
+        {
+            StartGame(8, 8, 10); // Легкий уровень
+        }
+
+        private void btnMedium_Click(object sender, EventArgs e)
+        {
+            StartGame(16, 16, 40); // Средний уровень
+        }
+
+        private void btnHard_Click(object sender, EventArgs e)
+        {
+            StartGame(24, 24, 99); // Сложный уровень
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // Выход из приложения
+        }
+
+        private void btnRules_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Правила игры: ...", "Правила"); // Показать правила игры
+        }
+
+        private void btnLeaderboard_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таблица лидеров: ...", "Таблица лидеров"); // Показать таблицу лидеров
+        }
+
         private void StartGame(int rows, int cols, int mines)
         {
             GameForm gameForm = new GameForm(rows, cols, mines);
             gameForm.Show();
+            this.Hide(); // Скрыть главную форму
         }
     }
 }
